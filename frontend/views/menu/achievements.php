95f609a130cb5e3ad7641d86474ae571a0a4de3c
TelegaStrasti <baigildin.idel@yandex.ru>
2023-01-23 20:59:49 +0500

    Починил таблицу

--- frontend/views/menu/achievements.php
+++ frontend/views/menu/achievements.php
@@ -2,6 +2,14 @@
     <div class="container">
         <dl class="structure__list">
             <dt class="structure__title"><?=$model->title?></dt>
+            <style>
+                tr{
+                    border-width: 1px !important;
+                }
+                td{
+                    border-width: 1px !important;
+                }
+            </style>
 <?php if ($model->text){
     echo $model->text;
 }else{
